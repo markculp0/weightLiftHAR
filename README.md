40d3707403e9af57204dc9a1f4915e0f9df32f76
Mark Culp <markculp0@gmail.com>
2017-05-29 20:46:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Human Activity Recognition (HAR) Weight Lifting Study
+## Analysis of Study Results
+## Mark Culp
+## May 29, 2017
+
---
+++ weightLiftHAR.R
@@ -0,0 +1,5 @@
+# ======================================================
+#  Weight Lifting, Human Activity Recognition (HAR)
+#  Machine Learning Project, Johns Hopkins
+#  Peer-graded Assignment: Prediction Assignment Writeup
+# ======================================================
