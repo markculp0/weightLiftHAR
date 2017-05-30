07bb442953e17abd685d8ae29c34824301e7fca3
Mark Culp <markculp0@gmail.com>
2017-05-29 21:21:10 -0500

    Load training set data; get user names.

--- weightLiftHAR.R
+++ weightLiftHAR.R
@@ -3,3 +3,11 @@
 #  Machine Learning Project, Johns Hopkins
 #  Peer-graded Assignment: Prediction Assignment Writeup
 # ======================================================
+
+## Load Training Set Data
+train <- read.csv("../data/pml-training.csv")
+
+## Get user names
+user <- unique(sort(train$user_name))
+
+
